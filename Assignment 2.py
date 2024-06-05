8cc21f520f530ac10ab3368ec6834daf9dbb3e9e
Ari Kotler <jansherremway@gmail.com>
2024-06-05 18:00:23 -0400

    Initial commit

---
+++ Assignment 2.py
@@ -0,0 +1,21 @@
+num1 = 10.5
+num2 = 3
+
+# a) Sum of the numbers
+print("Sum:", num1 + num2)
+
+# b) Difference of the numbers
+print("Difference:", num1 - num2)
+
+# c) Product of the numbers
+print("Product:", num1 * num2)
+
+# d) First number divided by the second number
+print("Division:", num1 / num2)
+
+# Bonus:
+# e) Num1 modulo num2
+print("Modulo:", num1 % num2)
+
+# f) Num1 raised to the power of num2
+print("Power:", num1 ** num2)
